fc3cb7313f97f696cfab053e9ca400eb957a3f0d
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-10 21:14:58 +0530

    feat: implement header navigation component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 
+import { Link } from "react-router-dom";
 
 function Header(){
-    return <></>
+    return (
+        <>
+        <header >
+            <h1 >ShoppyGlobe</h1>
+
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/cart'>Cart</Link>
+            </nav>
+        </header>
+    </>
+    )
 }
 
 export default Header ;
