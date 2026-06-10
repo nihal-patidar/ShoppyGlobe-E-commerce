5b807a072f08d27c384171075d79bacc9498e0f8
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-10 20:56:54 +0530

    feat : create application layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 function Layout(){
 
 
     return (
         <>
-        <Outlet />
+        <Header />
+        <main>
+            <Outlet />
+        </main>
         </>
     )
 }
